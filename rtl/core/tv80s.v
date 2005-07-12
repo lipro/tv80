74a0bc6afa5ae0be62f65c2d109315c9dc61a172
Guy Hutchison <ghutchis@opencores.org>
2005-07-12 16:36:17 +0000

    Changed default for T2Write to be 1, to match expected behavior for most users.
    git-svn-id: http://opencores.org/ocsvn/tv80/trunk@74 248d1722-e223-42e7-a16a-ebd4f5945538

--- rtl/core/tv80s.v
+++ rtl/core/tv80s.v
@@ -30,7 +30,7 @@ module tv80s (/*AUTOARG*/
   );
 
   parameter Mode = 0;    // 0 => Z80, 1 => Fast Z80, 2 => 8080, 3 => GB
-  parameter T2Write = 0; // 0 => wr_n active in T3, /=0 => wr_n active in T2
+  parameter T2Write = 1; // 0 => wr_n active in T3, /=0 => wr_n active in T2
   parameter IOWait  = 1; // 0 => Single cycle I/O, 1 => Std I/O cycle
 
 
